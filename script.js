4f780452bfa4a609f41c612cc2063995e47fc901
A-Kaliexe <145980980+A-Kaliexe@users.noreply.github.com>
2024-03-29 01:50:37 -0500

    Refactor code and added more accessibility to links, improve margin

--- script.js
+++ script.js
@@ -30,3 +30,23 @@ document.addEventListener("DOMContentLoaded", function () {
     }
   });
 });
+
+// Initialize Swiper when the DOM content is loaded
+document.addEventListener("DOMContentLoaded", function () {
+  const swiper = new Swiper(".swiper-container", {
+    // Optional parameters
+    loop: true,
+
+    // If we need pagination
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+
+    // Navigation arrows
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
+});
