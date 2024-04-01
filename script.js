c9cd991b854f7f42370927bdc8bd6f1011c08393
A-Kaliexe <145980980+A-Kaliexe@users.noreply.github.com>
2024-04-01 12:23:01 -0500

    Updated JS so nav bar functions

--- script.js
+++ script.js
@@ -1,17 +1,3 @@
-/* HOME PAGE - START (I forget what exactly it impacts) */
-
-$(document).ready(function () {
-  $(".box").mousemove(function (e) {
-    var window = $(this);
-    var x_axis = e.pageX - window.offset().left;
-    $(".mask").css({
-      left: x_axis,
-    });
-  });
-});
-
-/* HOME PAGE - END (I forget what exactly it impacts) */
-
 document.addEventListener("DOMContentLoaded", function () {
   // Function to toggle the visibility of the dropdown menu
   document.getElementById("menuButton").addEventListener("change", function () {
@@ -29,15 +15,5 @@ document.addEventListener("DOMContentLoaded", function () {
     } else {
       document.querySelector(".fixed-nav-bar").classList.remove("scrolled");
     }
-  });
-});
-//nav bar
-document.addEventListener("DOMContentLoaded", function () {
-  // Add event listener for menu button click
-  var menuButton = document.getElementById("menuButton");
-  var navMenu = document.getElementById("menu");
-
-  menuButton.addEventListener("click", function () {
-    navMenu.classList.toggle("dropped");
   });
 });
